3adfe2fb8252291c1958e701896ba163e069a852
bryan_alvarado18 <117047716+Bryan18-Alvarado@users.noreply.github.com>
2025-04-24 16:34:34 -0600

    creando seed para brands

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -2,14 +2,37 @@ import { Injectable } from '@nestjs/common';
 import { CarsService } from '../modules/cars/services/cars.service';
 import { initialData } from './data/seed-data';
 import { Car } from 'src/modules/cars/entities/car.entity';
+import { BrandsService } from 'src/modules/brands/services/brands.service';
+import { Brand } from 'src/modules/brands/entities/brand.entity';
 
 @Injectable()
 export class SeedService {
-  constructor(private readonly carsService: CarsService) {}
+  constructor(
+    private readonly carsService: CarsService,
+    private readonly brandsService: BrandsService,
+  ) {}
 
-  async runSeed() {
+  async runSeedCars() {
     await this.insertNewCars();
-    return 'SEED EXECUTED';
+
+    return 'SEED CARS EXECUTED';
+  }
+
+  async runSeedBrands() {
+    await this.insertNewBrands();
+
+    return 'SEED BRANDS EXECUTED';
+  }
+
+  private async insertNewBrands() {
+    await this.brandsService.deleteAllBrands();
+    const brands = initialData.brands;
+    const insertPromises: Promise<Brand | undefined>[] = [];
+    brands.forEach((brand) => {
+      insertPromises.push(this.brandsService.create(brand));
+    });
+
+    return true;
   }
 
   private async insertNewCars() {
